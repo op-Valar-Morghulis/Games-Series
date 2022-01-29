0f77aced0e8f6eaee304fdeafa178e3a6a55330d
op-Valar-MOrghulis <sdu.cjr@gmail.com>
2022-01-29 17:08:33 +0800

    update

--- Transformation/main.cpp
+++ Transformation/main.cpp
@@ -1,62 +1,28 @@
+// Assignment0 of Games101
+// author: Jiarui, Chen
+// Task of this code file
+// 给定一个点 P=(2,1), 将该点绕原点先逆时针旋转 45◦，再平移 (1,2), 计算出变换后点的坐标（要求用齐次坐标进行计算）
+
+#include<iostream>
 #include<cmath>
 #include<eigen3/Eigen/Core>
-#include<eigen3/Eigen/Dense>
-#include<iostream>
+
 
 int main(){
-    // // Basic Example of cpp
-    // std::cout << "Example of cpp \n";
-    // float a = 1.0, b = 2.0;
-    // std::cout << a << std::endl;
-    // std::cout << a/b << std::endl;
-    // std::cout << std::sqrt(b) << std::endl;
-    // std::cout << std::acos(-1) << std::endl;
-    // std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
-
-    // Example of vector
-    std::cout << "Example of vector \n";
-    // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
-    // // vector output
-    // std::cout << "Example of output \n";
-    // std::cout << v << std::endl;
-    // // vector add
-    // std::cout << "Example of add \n";
-    // std::cout << v + w << std::endl;
-    // // vector scalar multiply
-    // std::cout << "Example of scalar multiply \n";
-    // std::cout << v * 3.0f << std::endl;
-    // std::cout << 2.0f * v << std::endl;
-
-    // Example of matrix
-    std::cout << "Example of matrix \n";
-    // matrix definition
-    Eigen::Matrix3f i,j;
-    i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
-    j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
-    // // matrix output
-    // std::cout << "Example of output \n";
-    // std::cout << i << std::endl;
-    // matrix add i + j
-    // std::cout << "Practise: add matrix\n";
-    // std::cout << i+j << std::endl;
-    // // matrix scalar multiply i * 2.0
-    // std::cout << "Practise: scalar multiply of matrix\n";
-    // std::cout << 2.0f*i << std::endl;
-    // // matrix multiply i * j
-    // std::cout << "matrix multiply\n";
-    // std::cout << i*j << std::endl;
-    // // matrix multiply vector i * v
-    // std::cout << "matrix multiply vector\n";
-    // std::cout << i*v<< std::endl;
-
-    // given point p(2,1),rotate around origin
+    // Given point P = (2,1)
     Eigen::Vector3f p(2.0f,1.0f,1.0f);
     Eigen::Matrix3f transformation;
-    transformation << std::sin(45.0/180.0*acos(-1)), (-1)*std::cos(45.0/180.0*acos(-1)), 1, std::sin(45.0/180.0*acos(-1)), std::cos(45.0/180.0*acos(-1)) , 2, 0 ,0 , 1;
+    // acos(-1) = pi definition
+    transformation << std::cos(45.0/180.0*acos(-1)), (-1)*std::sin(45.0/180.0*acos(-1)), 1,
+                    std::sin(45.0/180.0*acos(-1)), std::cos(45.0/180.0*acos(-1)) , 2,
+                    0 , 0 , 1;
+    // Left multiply !!
     Eigen::Vector3f res = transformation*p;
     std::cout << "This is the result point\n";
     std::cout << res << std::endl;
     return 0;
 }
+
+// Question 1: Left hand coordinate / Right hand coordinate ? Difference?
+// Question 2: What is the meaning of 3rd value of point p? And why we define a vector but not a point ?
+// QUestion 3: How can know it is counterclockwise or clockwise ?
---
+++ Transformation/readme.md
@@ -0,0 +1,6 @@
+## Run option
+
+1. 'cd Transformation'
+2. 'cmake ..'
+3. 'make'
+4. './Transformation'
